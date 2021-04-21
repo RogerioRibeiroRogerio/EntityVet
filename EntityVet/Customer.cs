23c65d6fe744bc6115043303994ff3089eb31f68
Rogério Ribeiro Rogério <mundoroger@gmail.com>
2021-04-21 19:02:18 +0100

    Class Custumer

---
+++ EntityVet/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityVet
+{
+    class Customer
+    {
+
+        public string NameCustumer { get; set; }
+        public string EmailCustumer { get; set; }
+        public string AdressCustumer { get; set; }
+        public int IdCustumer { get; set; }
+        public int QntPetsCustumer { get; set; }
+
+        // public int IdPet { get; set; } (NAO INSIRO AQUI POIS POSSO TER VÀRIOS ANIMAIS PARA UMA PESSOA E NAO O CONTRARIO)  
+
+
+
+
+    }
+}
